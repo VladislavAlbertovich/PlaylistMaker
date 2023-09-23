722410b7b63eb7263f88531f52374ac0a5a1541f
VladislavAlbertovich <108926554+VladislavAlbertovich@users.noreply.github.com>
2023-09-23 14:05:27 +0300

    Revert "Домашнее задание, спринт 9"

--- app/src/main/java/com/example/playlistmarket/MainActivity.kt
+++ app/src/main/java/com/example/playlistmarket/MainActivity.kt
@@ -1,37 +1,11 @@
 package com.example.playlistmarket
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class  MainActivity : AppCompatActivity(){
-    @SuppressLint("MissingInflatedId")
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
-        val buttonLibrary = findViewById<Button>(R.id.buttonLibrary)
-        val buttonSettings = findViewById<Button>(R.id.buttonSettings)
-
-        buttonSearch.setOnClickListener{
-            val searchIntent = Intent(this, SearchActivity::class.java)
-            startActivity(searchIntent)
-        }
-
-        buttonLibrary.setOnClickListener{
-            val libraryIntent = Intent(this, MediaLibraryActivity::class.java)
-            startActivity(libraryIntent)
-        }
-
-        buttonSettings.setOnClickListener{
-            val settingsIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(settingsIntent)
-        }
-
-
     }
-
-
 }
