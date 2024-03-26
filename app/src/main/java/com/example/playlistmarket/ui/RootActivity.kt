212211a8790309514f34cc84ec4aee9b53f1a303
Vladislav <gudsanta66@gmail.com>
2024-03-26 22:02:07 +0300

    Спринт 19. Домашнее задание 1 попытка

---
+++ app/src/main/java/com/example/playlistmarket/ui/RootActivity.kt
@@ -0,0 +1,39 @@
+package com.example.playlistmarket.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.playlistmarket.R
+import com.example.playlistmarket.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.playerFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+
+                else -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/playlistmarket/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/example/playlistmarket/ui/player/PlayerFragment.kt
@@ -1,36 +1,39 @@
 package com.example.playlistmarket.ui.player
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmarket.R
-import com.example.playlistmarket.databinding.ActivityPlayerBinding
+import com.example.playlistmarket.databinding.FragmentPlayerBinding
 import com.example.playlistmarket.domain.search.models.Track
 import com.example.playlistmarket.presentation.player.PlayerViewModel
+import com.example.playlistmarket.ui.BindingFragment
 import com.example.playlistmarket.ui.player.Model.PlayerScreenState
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class PlayerActivity() : AppCompatActivity() {
+class PlayerFragment : BindingFragment<FragmentPlayerBinding>() {
 
-    private lateinit var binding: ActivityPlayerBinding
     private val playerViewModel by viewModel<PlayerViewModel>()
+    override fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentPlayerBinding {
+        return FragmentPlayerBinding.inflate(inflater)
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityPlayerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        playerViewModel.observeTrack().observe(this){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        playerViewModel.observeTrack().observe(viewLifecycleOwner) {
             bind(it)
         }
 
-
-
-        playerViewModel.observePlayerScreenState().observe(this) { state ->
+        playerViewModel.observePlayerScreenState().observe(viewLifecycleOwner) { state ->
             when (state) {
                 is PlayerScreenState.Paused -> {
                     binding.playButton.setImageResource(R.drawable.play_button)
@@ -49,7 +52,7 @@ class PlayerActivity() : AppCompatActivity() {
                 is PlayerScreenState.Playing -> {
                     binding.playButton.setImageResource(R.drawable.pause_button)
                     playerViewModel.startUpdateTime()
-                    playerViewModel.observeTime().observe(this) {
+                    playerViewModel.observeTime().observe(viewLifecycleOwner) {
                         binding.timeFragmentTextview.text = it
                     }
                 }
@@ -57,47 +60,12 @@ class PlayerActivity() : AppCompatActivity() {
         }
 
         binding.buttonBack.setOnClickListener {
-            finish()
+            findNavController().navigate(R.id.action_playerFragment_to_searchFragment)
         }
 
         binding.playButton.setOnClickListener {
             playerViewModel.playbackControl()
         }
-    }
-
-    private fun bind(track: Track) {
-        binding.apply {
-            trackName.text = track?.trackName
-            artistName.text = track?.artistName
-            durationValueTextView.text =
-                SimpleDateFormat("mm:ss", Locale.getDefault()).format(track?.trackTimeMillis)
-            if (track?.collectionName.isNullOrEmpty()) {
-                binding.albumValueTextView.text = track?.collectionName
-            } else {
-                binding.albumValueTextView.text = "-"
-            }
-            yearValueTextView.text =
-                SimpleDateFormat("yyyy", Locale.getDefault()).format(track?.releaseDate)
-            genreValueTextView.text = track?.primaryGenreName
-            countryValueTextView.text = track?.country
-            getCoverArtwork(track)
-        }
-    }
-
-    private fun getCoverArtwork(track: Track) {
-        Glide
-            .with(this)
-            .load(track?.artworkUrl512())
-            .placeholder(R.drawable.placeholder_album_in_player)
-            .centerCrop()
-            .transform(
-                RoundedCorners(
-                    binding.albumImageView.context.resources.getDimensionPixelOffset(
-                        R.dimen._8dp
-                    )
-                )
-            )
-            .into(binding.albumImageView)
     }
 
     override fun onResume() {
@@ -111,9 +79,43 @@ class PlayerActivity() : AppCompatActivity() {
         playerViewModel.onPause()
     }
 
-    override fun onDestroy() {
+    override fun onDestroyView() {
+        super.onDestroyView()
         playerViewModel.onDestroy()
-        super.onDestroy()
+    }
+
+    private fun bind(track: Track) {
+        binding.apply {
+            trackName.text = track.trackName
+            artistName.text = track.artistName
+            durationValueTextView.text =
+                SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis)
+            if (track.collectionName.isNullOrEmpty()) {
+                binding.albumValueTextView.text = track.collectionName
+            } else {
+                binding.albumValueTextView.text = "-"
+            }
+            yearValueTextView.text =
+                SimpleDateFormat("yyyy", Locale.getDefault()).format(track.releaseDate)
+            genreValueTextView.text = track.primaryGenreName
+            countryValueTextView.text = track.country
+            getCoverArtwork(track)
+        }
+    }
+
+    private fun getCoverArtwork(track: Track) {
+        Glide
+            .with(this)
+            .load(track.artworkUrl512())
+            .placeholder(R.drawable.placeholder_album_in_player)
+            .centerCrop()
+            .transform(
+                RoundedCorners(
+                    binding.albumImageView.context.resources.getDimensionPixelOffset(
+                        R.dimen._8dp
+                    )
+                )
+            )
+            .into(binding.albumImageView)
     }
 }
-
